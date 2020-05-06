c8546d7f9d14ebb8c5638b3ad78aef1e082b29da
Din Din <aminerdenebaynjargal@gmail.com>
2020-05-06 14:05:07 +0800

    Updates

---
+++ precache-manifest.b32768610a46de7922366a8a81687c6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281f38aed969f59bcc2eece363017f86",
+    "url": "/burgerr/index.html"
+  },
+  {
+    "revision": "a782a2ecbc98a4760589",
+    "url": "/burgerr/static/css/main.e33d8540.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burgerr/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burgerr/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a782a2ecbc98a4760589",
+    "url": "/burgerr/static/js/main.40d6f343.chunk.js"
+  },
+  {
+    "revision": "828696eec9c594ed86d9",
+    "url": "/burgerr/static/js/runtime-main.56053a6a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerr/static/media/burger-logo.b8503d26.png"
+  }
+]);
